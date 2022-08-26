5edd39181d9c970010958e2054d0d931d2646458
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-25 21:51:18 -0500

    Feat: Add index exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// * "Root" classes (starting points)
+export { BaseClient } from "./client/BaseClient";
+export { Client, default } from "./client/Client";
+
+// ! Errors
+export { iFunnyError } from "./errors/iFunnyError";
+export { iFunnyErrorCodes } from "./errors/iFunnyErrorCodes";
