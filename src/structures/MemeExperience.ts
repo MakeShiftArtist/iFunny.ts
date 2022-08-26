d069d77ea6944dbd17c75bec75b8ef998a57341c
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-25 22:19:31 -0500

    Feat: Add MemeExperience

---
+++ src/structures/MemeExperience.ts
@@ -0,0 +1,49 @@
+import { APIMemeExperience } from "@ifunny/ifunny-api-types";
+
+/**
+ * The Meme Experience for a user
+ */
+export class MemeExperience {
+	protected data: APIMemeExperience;
+	constructor(data: APIMemeExperience) {
+		this.data = data;
+	}
+
+	/**
+	 * The user's rank
+	 */
+	get rank() {
+		return this.data.rank;
+	}
+
+	/**
+	 * How many days the user has spent logged in
+	 */
+	get days() {
+		return this.data.days;
+	}
+
+	/**
+	 * Url of the badge
+	 * @example
+	 * `https://img.ifunny.co/meme_experience/${number}.png`
+	 */
+	get badgeUrl() {
+		return this.data.badge_url;
+	}
+
+	/**
+	 * Size of the badge
+	 */
+	get badgeSize() {
+		return this.data.badge_size;
+	}
+
+	/**
+	 * Days needed to get the next rank\
+	 * ? Chef's Meme Agent will show 3000 but does not change when it reaches it
+	 */
+	get nextMilestone() {
+		return this.data.next_milestone;
+	}
+}
