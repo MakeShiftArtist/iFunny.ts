edf6e5f4560102abf63e009ce6bbcc5013af83a7
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-28 18:38:03 -0500

    Fix: Fixed UserManager

--- src/managers/UserManager.ts
+++ src/managers/UserManager.ts
@@ -1,9 +1,38 @@
+import {
+	APIUser,
+	Endpoints,
+	RESTAPISuccessResponse as Success,
+} from "@ifunny/ifunny-api-types";
+
 import { CachedManager } from "./CachedManager";
 import { Client } from "../client/Client";
 import { User } from "../structures/User";
 
-export class UserManager extends CachedManager<User> {
+export class UserManager extends CachedManager<typeof User> {
 	constructor(client: Client) {
-		super(client);
+		super(client, User);
+	}
+
+	/**
+	 * @param idOrNick Id or nick of the user
+	 * @param byNick Whether to lookup by nick
+	 * @param cached Whater to return the cached result
+	 * @returns
+	 */
+	public async fetch(
+		idOrNick: string,
+		byNick: boolean = false,
+		cached: boolean = true
+	) {
+		let user: User | null;
+		if (cached) user = this.resolve(idOrNick);
+		let { data } = await this.client.instance.request<Success<APIUser> | null>({
+			url: Endpoints.user(idOrNick, byNick),
+		});
+		if (!data) return data;
+		user = new User(this.client, data.data.id, data.data);
+		this.cache.set(user.id, user);
+		this.cache.set(user.nick!, user);
+		return user;
 	}
 }
