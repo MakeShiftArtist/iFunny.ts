57830a5547b47f249a2c12a74708e41609509256
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-28 18:39:16 -0500

    Fix: Removed useless fetch override from User

--- src/structures/User.ts
+++ src/structures/User.ts
@@ -1,22 +1,8 @@
 import { BaseUser } from "./BaseUser";
 import { Client } from "../client/Client";
 
-interface TimelineOptions {
-	limit?: number;
-	original?: boolean;
-	next?: string;
-}
-
 export class User extends BaseUser {
 	constructor(client: Client, id: string, payload?: any) {
 		super(client, id, payload);
-	}
-
-	/**
-	 * Fetches new data about the user from the API
-	 * @returns The User instance
-	 */
-	async fetch() {
-		return super.fetch();
 	}
 }
