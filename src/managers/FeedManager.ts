9280f0e6bf936d532aa4d02b190f9c85c6f1da56
MakeShiftArtist <47653736+MakeShiftArtist@users.noreply.github.com>
2025-12-14 18:20:07 -0600

    style(formatting): run prettier and convert @alias to @link

--- src/managers/FeedManager.ts
+++ src/managers/FeedManager.ts
@@ -5,54 +5,63 @@ import { Feed } from "../structures/Feed";
  * Handles different feeds for the Client
  */
 export class FeedManager {
-	/**
-	 * Client attached to the FeedManager
-	 */
-	readonly #client: Client;
-	readonly #features: Feed;
-	readonly #collective: Feed;
-	readonly #home: Feed;
+    /**
+     * {@link Client} attached to the FeedManager
+     */
+    readonly #client: Client;
+    readonly #features: Feed;
+    readonly #collective: Feed;
+    readonly #home: Feed;
 
-	/**
-	 * @param client Client instance
-	 */
-	public constructor(client: Client) {
-		this.#client = client;
-		this.#features = new Feed(this.#client, Feed.Types.FEATURES);
-		this.#collective = new Feed(this.#client, Feed.Types.COLLECTIVE);
-		this.#home = new Feed(this.#client, Feed.Types.HOME);
-	}
+    /**
+     * @param client Client instance
+     */
+    public constructor(client: Client) {
+        this.#client = client;
+        this.#features = new Feed(this.#client, Feed.Types.FEATURES);
+        this.#collective = new Feed(this.#client, Feed.Types.COLLECTIVE);
+        this.#home = new Feed(this.#client, Feed.Types.HOME);
+    }
 
-	/**
-	 * The featured feed
-	 */
-	public get features(): Feed {
-		return this.#features;
-	}
+    /**
+     * The {@link Client} instance
+     */
+    public get client(): Client {
+        return this.#client;
+    }
 
-	/**
-	 * The collective feed
-	 */
-	public get collective(): Feed {
-		return this.#collective;
-	}
+    /**
+     * The featured feed
+     */
+    public get features(): Feed {
+        return this.#features;
+    }
 
-	/**
-	 * The subscriptions feed
-	 * @alias home
-	 */
-	public get subscriptions(): Feed {
-		return this.home;
-	}
+    /**
+     * The collective feed
+     */
+    public get collective(): Feed {
+        return this.#collective;
+    }
 
-	/**
-	 * The home feed
-	 * @alias subscriptions
-	 */
-	public get home(): Feed {
-		// This is the url so I included it
-		return this.#home;
-	}
+    /**
+     * The subscriptions feed
+     *
+     * Alias for {@link home}
+     */
+    public get subscriptions(): Feed {
+        return this.home;
+    }
+
+    /**
+     * The home feed
+     *
+     * Alias for {@link subscriptions}
+     */
+    public get home(): Feed {
+        // This is the url so I included it
+        return this.#home;
+    }
 }
 
 export default FeedManager;
