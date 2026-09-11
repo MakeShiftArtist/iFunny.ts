8076b630d4e5425a51e7b06213a27f471124dcce
MakeShiftArtist <47653736+MakeShiftArtist@users.noreply.github.com>
2026-09-10 23:07:01 -0500

    chore(devenv): update devenv to latest version

--- devenv.nix
+++ devenv.nix
@@ -7,58 +7,22 @@
 }:
 
 {
-  # Set environment variables here
-  # https://devenv.sh/basics/
-  # env.GREET = "devenv";
 
   # https://devenv.sh/packages/
   packages = [
     pkgs.git
-    pkgs.nixfmt-rfc-style
+    pkgs.nixd
+    pkgs.nixfmt
   ];
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-  # languages.deno.enable = true;
   languages.nix.enable = true;
 
   languages.javascript = {
     enable = true;
     npm.enable = true;
-    package = pkgs.nodejs_25;
+    nodejs.enable = true;
     bun.enable = true;
   };
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  # scripts.hello.exec = ''
-  #   echo hello from $GREET
-  # '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
